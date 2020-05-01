492d09bf07706263f514a23bd41bff2e569a8ef9
uuta <yuta.morishita0916gmail.com>
2020-05-01 22:16:12 +0900

    class view change

--- blog/views.py
+++ blog/views.py
@@ -4,20 +4,25 @@ from .models import Blog
 from django.contrib import messages
 from django.db.models import Q
 
-
-def index(request):
-    blog = Blog.objects.order_by('-id')
-    """ 検索機能の処理 """
-    keyword = request.GET.get('keyword')
-    if keyword:
-        """ テキスト用のQオブジェクトを追加 """
-        blog = blog.filter(
-            Q(title__icontains=keyword) | Q(text__icontains=keyword)
-        )
-        messages.success(request, '「{}」の検索結果'.format(keyword))
-    return render(request, 'blog/index.html', {'blog': blog})
+from django.views import generic
 
 
-def detail(request, blog_id):
-    blog_text = get_object_or_404(Blog, id=blog_id)
-    return render(request, 'blog/detail.html', {'blog_text': blog_text})
+class IndexView(generic.ListView):
+    model = Blog
+    template_name = 'blog/index.html'
+    """ オーバーライド """
+
+    def get_queryset(self):
+        queryset = Blog.objects.order_by('-id')
+        keyword = self.request.GET.get('keyword')
+        if keyword:
+            queryset = queryset.filter(
+                Q(title__icontains=keyword) | Q(text__icontains=keyword)
+            )
+            messages.success(self.request, '「{}」の検索結果'.format(keyword))
+        return queryset
+
+
+class DetailView(generic.DeleteView):
+    template_name = 'blog/detail.html'
+    model = Blog
