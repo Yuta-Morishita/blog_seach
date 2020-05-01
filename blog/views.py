4e2902c0e5c107c6ad1c5a24667ac17e0d912a69
uuta <yuta.morishita0916gmail.com>
2020-05-02 01:03:49 +0900

    seach_multiple

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,33 @@
-from django.shortcuts import render, get_object_or_404
+
 from .models import Blog
 
 from django.contrib import messages
 from django.db.models import Q
-
-from django.views import generic
+from django.views import generic  # クラスリストビュー用
+""" 追加 """
+from functools import reduce
+from operator import and_
 
 
 class IndexView(generic.ListView):
     model = Blog
     template_name = 'blog/index.html'
-    """ オーバーライド """
 
     def get_queryset(self):
         queryset = Blog.objects.order_by('-id')
         keyword = self.request.GET.get('keyword')
         if keyword:
-            queryset = queryset.filter(
-                Q(title__icontains=keyword) | Q(text__icontains=keyword)
+            exclusion = set([' ', '　'])
+            q_list = ''
+            for i in keyword:
+                if i in exclusion:
+                    pass
+                else:
+                    q_list += i
+            query = reduce(
+                and_, [Q(title__icontains=q) | Q(text__icontains=q) for q in q_list]
             )
+            queryset = queryset.filter(query)
             messages.success(self.request, '「{}」の検索結果'.format(keyword))
         return queryset
 
