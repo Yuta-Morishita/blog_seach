6706116af5fca3f21e2d2269c865ef18e740ec72
uuta <yuta.morishita0916gmail.com>
2020-05-01 20:23:17 +0900

    third commit

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
+from django.contrib import messages
+from django.db.models import Q
+
 
 def index(request):
     blog = Blog.objects.order_by('-id')
+    """ 検索機能の処理 """
+    keyword = request.GET.get('keyword')
+    if keyword:
+        """ テキスト用のQオブジェクトを追加 """
+        blog = blog.filter(
+            Q(title__icontains=keyword) | Q(text__icontains=keyword)
+        )
+        messages.success(request, '「{}」の検索結果'.format(keyword))
     return render(request, 'blog/index.html', {'blog': blog})
 
 
